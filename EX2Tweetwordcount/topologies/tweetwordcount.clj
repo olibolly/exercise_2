6f4d5390347f3aef50cf532e8a40b4b066578633
Olivier Zimmer <ozimmer@ozimmer-macbookpro.roam.corp.google.com>
2016-07-23 12:55:40 -0400

    debugging

--- EX2Tweetwordcount/topologies/tweetwordcount.clj
+++ EX2Tweetwordcount/topologies/tweetwordcount.clj
@@ -5,23 +5,24 @@
 (defn tweetwordcount [options]
    [
     ;; spout configuration
-    {"tweet-spout" (python-spout-spec
+   {"sentences" (python-spout-spec
           options
           "spouts.tweetsTest.Sentences"
           ["sentence"]
           )
     }
-    ;; bolt configuration
-    {"parse-tweet-bolt" (python-bolt-spec
+    ;; bolt configuration 1
+    {"parse" (python-bolt-spec
           options
-          {"tweet-spout" :shuffle}
+          {"sentences" :shuffle}
           "bolts.parseTest.ParseTweet"
           ["valid_words"]
           :p 2
           )
-    "count-bolt" (python-bolt-spec
+    ;; bolt configuration 2
+    "tweetcounter" (python-bolt-spec
           options
-          {"parse-tweet-bolt" :shuffle}
+          {"parse" :shuffle}
           "bolts.wordcountTest.TweetCounter"
           ["word" "count"]
           :p 1
