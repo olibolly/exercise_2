5bd8008258b068af239d86a77291b56860bba946
Olivier Zimmer <ozimmer@ozimmer-macbookpro.roam.corp.google.com>
2016-07-23 11:36:14 -0400

    test tweets.py

--- EX2Tweetwordcount/src/spouts/tweets.py
+++ EX2Tweetwordcount/src/spouts/tweets.py
@@ -75,6 +75,8 @@ class Tweets(Spout):
             if tweet:
                 self.queue().task_done()
                 self.emit([tweet])
+                #testing the script
+                self.log(tweet)
  
         except Queue.Empty:
             self.log("Empty queue exception ")
